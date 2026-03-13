59903184f5f6e8d9d86d409c696acac630b81c25
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-13 02:22:38 -0300

    horas para fazer um for, sem ia é trabalhoso

--- validator/main.go
+++ validator/main.go
@@ -5,12 +5,19 @@ import (
 	"regexp"
 )
 
-
-func main() {
-
-	cpf := "529.982.247-25"
+func validateFirstDigit(cpf string) {
 	cleanCpf := regexp.MustCompile(`[^\d]+`).ReplaceAllString(cpf, "")
 	fmt.Println(cleanCpf)
+	sum := 0
+	for i := 0; i < len(cleanCpf); i++ {
+		fmt.Printf("%c \n", cleanCpf[i])
+	}
+
+	println(sum)
+}
+
+func main() {
+	validateFirstDigit("529.982.247-25")
 }
 
 // \d
@@ -19,7 +26,6 @@ func main() {
 // []
 // [^]
 // +
-
 
 // func validatorFistDigit(cpf string) string {
 // 	// cpf = "529.982.247-25"
