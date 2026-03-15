56b96a2b36d6cd438877a721f28f2ba861343b4d
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-15 20:01:44 -0300

    removendo caracteress

--- validator/main.go
+++ validator/main.go
@@ -5,42 +5,15 @@ import (
 	"regexp"
 )
 
-func validateFirstDigit(cpf string) {
-	cleanCpf := regexp.MustCompile(`[^\d]+`).ReplaceAllString(cpf, "")
-	fmt.Println(cleanCpf)
-	sum := 0
-	for i := 0; i < len(cleanCpf); i++ {
-		fmt.Printf("%c \n", cleanCpf[i])
-	}
-
-	println(sum)
+func validaCpf(cpf string) bool {
+	re := regexp.MustCompile(`[^\d]+`)
+	cpf = re.ReplaceAllString(cpf, "")
+	fmt.Println(cpf)
+	return false
 }
 
 func main() {
-	validateFirstDigit("529.982.247-25")
+	    //  529 982 247 25
+	cpf := "529.982.247-25" 
+	validaCpf(cpf)
 }
-
-// \d
-// \w
-// \s
-// []
-// [^]
-// +
-
-// func validatorFistDigit(cpf string) string {
-// 	// cpf = "529.982.247-25"
-// 	// fmt.Println(cpf)
-// 	// 	var sum int
-// 	// 	println(sum)
-
-// 	//	for i := 0; i < 9; i++ {
-// 	//		// result :=
-// 	//	}
-// 	//
-// 	// //    sum += result * (10 - i)
-// 	//
-// 	//	// (sum * 10) % 11
-// 	fmt.Println(cpf)
-// 	return cpf
-
-// }
