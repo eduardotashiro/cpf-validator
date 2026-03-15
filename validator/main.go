6a3ee02422fde3242ee5ba9a757f1bd316a69530
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-15 20:44:58 -0300

    ...

--- validator/main.go
+++ validator/main.go
@@ -6,14 +6,30 @@ import (
 )
 
 func validaCpf(cpf string) bool {
-	re := regexp.MustCompile(`[^\d]+`)
-	cpf = re.ReplaceAllString(cpf, "")
-	fmt.Println(cpf)
-	return false
+	cleanCpf := regexp.MustCompile(`[^\d]+`).ReplaceAllString(cpf, "")
+
+	if len(cleanCpf) != 11 {
+		return false
+	}
+
+	// for _, l := range cleanCpf {
+	// 	fmt.Println(string(l))
+	// }
+	sum := 0
+	for i := 0; i < 9; i++ {
+		sum += int(cleanCpf[i]) * (10 - i)
+		// resto := (sum * 10) % 11
+		fmt.Println(sum)
+	}
+
+	// resto := (sum * 10) % 11
+	// 	fmt.Println(resto)
+
+	return true //vai retornar true quando for valido
 }
 
 func main() {
 	    //  529 982 247 25
-	cpf := "529.982.247-25" 
+	cpf := "529.982.247-25"
 	validaCpf(cpf)
 }
