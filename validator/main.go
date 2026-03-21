3b1f6a0a6b9cc39659d1a76278ac6dd15c5850e2
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-20 22:35:49 -0300

    simplificando

--- validator/main.go
+++ validator/main.go
@@ -2,81 +2,51 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
-var (
-	max int8 = 14 // digitos mais os caracteres
-	div int8 = 11 // digitos
-)
+func removeCharacters(cleanCPF string) string {
+	re := regexp.MustCompile(`[^\d]+`)
+	cpf := re.ReplaceAllString(cleanCPF, "")
+	return cpf
+}
 
-func calculaDigitos(cpf []int, a int) {
-	var (
-		b      int = a - 1
-		i      int
-		dig    int 
-		soma   int 
-		result int 
-		rest   int 
-	)
+// func isRepetitive(repCPF string) string {
+// 	if len(repCPF) == 0 {
+// 		fmt.Printf("cpf : %s , vazio !!", repCPF)
+// 	}
+// 	if repCPF[0] == repCPF[1] && repCPF[1] == repCPF[2] && repCPF[2] == repCPF[3] && repCPF[3] == repCPF[4] && repCPF[4] == repCPF[5] && repCPF[5] == repCPF[6] && repCPF[6] == repCPF[7] && repCPF[7] == repCPF[8] && repCPF[8] == repCPF[9] && repCPF[9] == repCPF[10] && repCPF[10] == repCPF[0] {
+// 		fmt.Printf("cpf : %s , não pode repetir os números !!", repCPF)
+// 	}
+// 	return repCPF
+// }
 
-	for i = 0; i < b; i++ {
-		
+func validateCPF(c string) bool {
+	cpf := removeCharacters(c)
+	if len(cpf) != 11 {
+		return false
 	}
+
+	sum := 0
+	for i := range 9 {
+		digit, err := strconv.Atoi(string(cpf[i]))
+		if err != nil {
+			return false
+		}
+		sum += digit * (10 - i)
+	}
+
+	rest1 := (sum * 10) % 11
+	if rest1 < 10 {
+		return rest1 == int(cpf[9])
+	}
+	return int(cpf[9] == 0)
+
 }
-
-func validador(a int, b int, c[]int){
-
-
-}
-
-
 
 func main() {
-
+	//  529 982 247 25
+	cpf := "529.982.247-25"
+	validateCPF(cpf)
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"regexp"
-// 	"strconv"
-// )
-
-// func validaCpf(cpf string) bool {
-// 	re := regexp.MustCompile(`[^\d]+`)
-// 	cpf = re.ReplaceAllString(cpf, "")
-// 	if len(cpf) != 11 {
-// 		return false
-// 	}
-
-// 	sum := 0
-// 	for i := range 9 {
-// 		digit, err := strconv.Atoi(string(cpf[i]))
-// 		if err != nil {
-// 			return false
-// 		}
-// 		sum += digit * (10 - i)
-// 	}
-// 	r := (sum * 10) % 11
-// 	string(r)
-// 	if r == cpf[9]{
-
-// 	}
-
-// 	// resto := (sum * 10) % 11
-// 	fmt.Println(sum) //295 :)
-// 	// }
-// 	// resto := (sum * 10) % 11
-// 	// if resto < 10 {
-// 	// 	return int(cleanCpf[9]) == resto
-// 	// }
-// 	return true
-// 	//int(cleanCpf[9]) == 0
-// }
-
-// func main() {
-// 	//  529 982 247 25
-// 	cpf := "529.982.247-25"
-// 	validaCpf(cpf)
-// }
